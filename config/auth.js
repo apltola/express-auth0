b0b8ee188a6fffc6b229c350838a761d27d9318d
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2021-03-06 16:45:20 +0200

    adding auth0 and react client

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+module.exports = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN
+}
--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const helmet = require('helmet')
+const { auth } = require('express-openid-connect')
+const { requiresAuth } = require('express-openid-connect')
+const authConfig = require('./config/auth')
 
 const app = express()
 app.use(cors())
-app.use(helmet())
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+)
 
-app.get('/', (req, res) => {
-  res.send('hello hello')
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(authConfig))
+
+// req.isAuthenticated is provided from the auth router
+/* app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+}) */
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user))
 })
 
-app.get('/html', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'html', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  const buildPath = path.resolve(__dirname, 'client', 'build')
+  app.use('/', express.static(buildPath))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(buildPath, 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`server listening at ${PORT}`))
-
