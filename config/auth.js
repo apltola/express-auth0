40b39125681bb820a55a66976fbacfb852dc1797
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2021-03-06 17:00:18 +0200

    update envs

--- config/auth.js
+++ config/auth.js
@@ -8,5 +8,5 @@ module.exports = {
   secret: process.env.AUTH0_CLIENT_SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_DOMAIN
+  issuerBaseURL: process.env.AUTH0_BASE_URL
 }
